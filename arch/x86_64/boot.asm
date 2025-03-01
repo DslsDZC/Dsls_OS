589674afe05db83bb07ae7dd16968433a892f364
DslsDZC <2470305344@qq.com>
2025-03-01 14:53:23 +0900

    feat(x86_64): 改进启动过程并初始化中断
    - 在进入保护模式前设置 A20 线
    - 初始化数据段寄存器和中断描述符表
    - 添加简单的显存写入测试
    - 修复全局描述符表的定义
    - 优化代码结构和格式

--- arch/x86_64/boot.asm
+++ arch/x86_64/boot.asm
@@ -3,6 +3,10 @@
 
 start:
     cli
+    in al, 0x92
+    or al, 2
+    out 0x92, al
+    
     lgdt [gdt_desc]
     mov eax, cr0
     or al, 1
@@ -13,19 +17,41 @@ start:
 protected_mode:
     mov ax, 0x10
     mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
     mov ss, ax
-    mov esp, 0x90000
+    mov esp, 0x7A00
+    lidt [idt_desc]
+    mov byte [0xB8000], 'A'
+    mov byte [0xB8001], 0x0F
     jmp kernel_main
+    cli
+    hlt
+    jmp $
 
 gdt:
     dq 0x0
-    dw 0xFFFF, 0x0
-    db 0x0, 0x9A, 0xCF, 0x0
-    dw 0xFFFF, 0x0
-    db 0x0, 0x92, 0xCF, 0x0
+    dw 0xFFFF
+    dw 0x0000
+    db 0x00
+    db 0x9A
+    db 0xCF
+    db 0x00
+    dw 0xFFFF
+    dw 0x0000
+    db 0x00
+    db 0x92
+    db 0xCF
+    db 0x00
+
 gdt_desc:
     dw $ - gdt - 1
     dd gdt
+
+idt_desc:
+    dw 0
+    dd 0
 
 times 510 - ($ - $$) db 0
 dw 0xAA55
