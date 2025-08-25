abe281a363077a7014b91d1159002d39d0f40231
DslsDZC <dsls.dzc@gmail.com>
2025-08-25 17:46:57 +0800

    uefi updata 2
    更新 uefi.h 文件，添加新功能和字段
    
    在 `uefi.h` 中添加了对 `elf.h` 和 `Protocol/Security2.h` 的包含。更新了 `boot_params_t` 结构体，移除了注释并添加了 `acpi_rsdp`、`cmdline` 和 `crc32` 字段。新增了多个函数声明以支持内核签名验证、内存保护、内存映射验证等功能。

--- arch/x86_64/boot/uefi/uefi.h
+++ arch/x86_64/boot/uefi/uefi.h
@@ -3,6 +3,8 @@
 
 #include <efi.h>
 #include <efilib.h>
+#include <elf.h>
+#include <Protocol/Security2.h>
 
 // 图形模式信息结构
 typedef struct {
@@ -27,12 +29,21 @@ typedef struct {
 typedef struct {
     graphics_info_t graphics;
     memory_map_info_t memory_map;
-    // 可以添加其他启动参数，如ACPI表等
+    VOID* acpi_rsdp;              // ACPI根系统描述指针
+    CHAR16 cmdline[256];          // 内核命令行参数
+    UINT32 crc32;                 // 引导参数CRC32校验和
 } boot_params_t;
 
 // 函数声明
 EFI_STATUS EFIAPI uefi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE* SystemTable);
 EFI_STATUS init_graphics(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE* SystemTable, graphics_info_t* gfx_info);
 EFI_STATUS load_kernel(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE* SystemTable, VOID** kernel_entry_point);
+EFI_STATUS verify_kernel_signature(EFI_SYSTEM_TABLE* SystemTable, VOID* kernel_data, UINTN kernel_size);
+EFI_STATUS set_kernel_memory_protection(EFI_SYSTEM_TABLE* SystemTable, Elf64_Ehdr* elf_header, EFI_PHYSICAL_ADDRESS kernel_address);
+EFI_STATUS validate_memory_map(EFI_MEMORY_DESCRIPTOR* memory_map, UINTN memory_map_size, UINTN descriptor_size);
+UINT32 calculate_boot_params_crc(boot_params_t* boot_params);
+EFI_STATUS get_acpi_tables(EFI_SYSTEM_TABLE* SystemTable, VOID** rsdp);
+EFI_STATUS get_kernel_command_line(EFI_SYSTEM_TABLE* SystemTable, CHAR16* cmdline, UINTN size);
+EFI_STATUS setup_kernel_stack(EFI_SYSTEM_TABLE* SystemTable, EFI_PHYSICAL_ADDRESS* stack_top);
 
 #endif // UEFI_H
